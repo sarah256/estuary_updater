bb9252fafe9d342f15e4cf1a0f278c2b3db1db30
sarah256 <srieger@bu.edu>
2018-11-02 15:58:23 -0400

    Added mock import for koji in docs, because Readthedocs.io does not support it

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,8 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+autodoc_mock_imports = ['koji']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
